fea467e56bdff2bea557abd87ad2c065b4c7ae7e
nilovlev <nilovlev@yandex.ru>
2025-01-31 15:15:57 +0300

    Basic setup

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # 3D-Renderer
+
+How to run:
+
+`mkdir build`
+
+`cd build`
+
+`cmake ..`
+
+`make`
+
+`./3D-Renderer`
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "except.h"
+
+int main() {
+  try {
+    
+  } catch(...) {
+    except::react();
+  }
+  return 0;
+}
